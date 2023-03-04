6aa8a8d3b9c08334aa0a414456faa54e1e9a1358
Your computadorescol <computadorescol@gmail.com>
2023-03-04 18:00:14 -0500

    paso 42

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import './sass/App.scss';
 import { TiArrowLeftOutline} from "react-icons/ti";
 import { TiArrowRightOutline} from "react-icons/ti";
 //hooks
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 
 
 const App =()=> {
   const  [pokemonId, setPokemonId] = useState(1);
-  
+
+     useEffect( ()=>{
+        getEvolutions(pokemonId);
+           }
+         )  
+     async function getEvolutions(id) {
+        const response =  await fetch(`https://pokeapi.co/api/v2/evolution-chain/{id}/ `)
+        const data = await response.json()
+        console.log(data.chain.species.name)            
+     }
+     
+
   function prevClick () {
     (pokemonId == 1 ) ?  
                   setPokemonId(1) :
