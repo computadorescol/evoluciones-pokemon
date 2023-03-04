5cbe21c920432d3f2653f82b1340a3c5f3b6f3ce
Your computadorescol <computadorescol@gmail.com>
2023-03-03 20:28:26 -0500

    paso 32

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import {Button} from "./components/Button"
 import './sass/App.scss';
 import { TiArrowLeftOutline} from "react-icons/ti";
 import { TiArrowRightOutline} from "react-icons/ti";
+import { useState } from "react";
 
 
 
-
-function App() {
-
+const App =()=> {
+  const  [  pokemonId, setPokemonId ] = useState(1)
   return (
     <> 
     {/*las tarjetas van aqui*/}
     <div className= "buttons-container">
       
     <Button 
-     icon ={< TiArrowLeftOutline/>} handleClick={()=>{console.log('anterior')}} />
+     icon ={< TiArrowLeftOutline/>} 
+     handleClick={()=>{setPokemonId, (pokemonId -1 )}} />
+    {pokemonId}
     
-    
-     <Button icon ={< TiArrowRightOutline/>} handleClick={()=>{console.log('siguiente')}} />
+     <Button icon ={< TiArrowRightOutline/>} handleClick={()=>{setPokemonId, (pokemonId + 1 )}} />
      
     </div>
     </>
