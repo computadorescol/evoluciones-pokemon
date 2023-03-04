79f9171c5cf578185e6418500743599af1599476
Your computadorescol <computadorescol@gmail.com>
2023-03-04 16:35:48 -0500

    paso 41

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import    "../sass/Card.scss";
 const Card = ()=> {
   return (
     <div className= "card"  >
-		 <p  className = "card__name "  > David   </p>
+		 <p  className = "card__name "  > Pokemon   </p>
 		 <div className= "card__circle "> </div>
 		 <img className = "card__img " src=" " alt = "pokemon img " /> </div>
   )
