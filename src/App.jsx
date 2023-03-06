e8c1b919b9a31bde0eecdf34e17df05af33fbbf8
Your computadorescol <computadorescol@gmail.com>
2023-03-06 10:57:50 -0500

    STEP  42 .

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,16 @@ import { useState , useEffect } from "react";
 
 
 const App =()=> {
-  const  [pokemonId, setPokemonId] = useState(1);
+  const  [pokemonId, setPokemonId] = useState(10);
 
-     useEffect( ()=>{
+     useEffect( () => {
         getEvolutions(pokemonId);
            }
          )  
      async function getEvolutions(id) {
-        const response =  await fetch(`https://pokeapi.co/api/v2/evolution-chain/{id}/ `)
+        const response =  await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
         const data = await response.json()
-        console.log(data.chain.species.name)            
+        console.log(data)          
      }
      
 
