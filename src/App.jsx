65114dd8038bb088caa409fbeeb2066e84fdcc59
Your computadorescol <computadorescol@gmail.com>
2023-03-06 20:54:17 -0500

    paso 43.3.3.3

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import './sass/App.scss';
 import { TiArrowLeftOutline} from "react-icons/ti";
 import { TiArrowRightOutline} from "react-icons/ti";
 //hooks
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 
 
 const App =()=> {
-  const  [pokemonId, setPokemonId] = useState(1);
-  
+  const  [pokemonId, setPokemonId] = useState(10);
+  const [pokemonName, setPokemonName ] = useState (' ') ;
+     useEffect( () => {
+        getEvolutions(pokemonId);
+           }
+         )  
+     async function getEvolutions(id) {
+        const response =  await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
+        const data = await response.json()
+        setPokemonName(data.chain.species.name)          
+     }
+     
+
   function prevClick () {
     (pokemonId == 1 ) ?  
                   setPokemonId(1) :
@@ -33,7 +44,8 @@ const App =()=> {
      icon ={< TiArrowLeftOutline/>} 
      handleClick={prevClick } />
     
-    {pokemonId}
+    
+    {pokemonName}
     
      <Button icon ={< TiArrowRightOutline/>}
       handleClick={nextClick } />
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from "./App" ;
+import { App } from "./App" 
 import  "./sass/index.scss"
 
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector ('#root'))
 root.render(<App />)
 console.log()
