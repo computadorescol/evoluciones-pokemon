66f320545f1e8fdd86ef6efe1a472f7479875a2c
Your computadorescol <computadorescol@gmail.com>
2023-03-06 21:06:29 -0500

    step 43 after git merge

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,18 @@ import { useState , useEffect } from "react";
 
 
 const App =()=> {
-  const  [pokemonId, setPokemonId] = useState(1);
   
+  const  [pokemonId, setPokemonId] = useState(10);
+  const [pokemonName, setPokemonName ] = useState (' ') ;
+     useEffect( () => {
+        getEvolutions(pokemonId);
+           }
+         )  
+     async function getEvolutions(id) {
+        const response =  await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
+        const data = await response.json()
+        setPokemonName(data.chain.species.name)          
+     }
   function prevClick () {
     (pokemonId == 1 ) ?  
                   setPokemonId(1) :
